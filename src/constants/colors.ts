b101189d60362f2bd46681d3cb278852fd022208
Datto <dato.getsadze7@gmail.com>
2024-02-21 16:31:27 +0400

    init DrawerNavigator

---
+++ src/constants/colors.ts
@@ -0,0 +1,19 @@
+export const colorsDark = {
+  primary: '#11244A',
+  secondary: '#0B1C3E',
+  tertiary: '#061831',
+  background: '#030A1C',
+  text1: 'white',
+  text2: '#e8e6e6',
+  text3: '#787878',
+};
+
+export const colorsLight = {
+  primary: '#11244A',
+  secondary: '#0B1C3E',
+  tertiary: '#061831',
+  background: '#030A1C',
+  text1: 'white',
+  text2: '#e8e6e6',
+  text3: '#787878',
+};
